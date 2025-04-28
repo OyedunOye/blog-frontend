ebbfb87769df1ea9990d2111844fff14bc4e3f04
OyedunOye <oyesinaoyedun@yahoo.com>
2025-04-28 23:41:08 +0200

    update: Added title and description metadata to the layout.tsx for this page.

--- app/create-blog/layout.tsx
+++ app/create-blog/layout.tsx
@@ -1,5 +1,12 @@
 import Footer from "@/components/common/Footer";
 import NavBar from "@/components/common/NavBar";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Blog's creation page",
+  description:
+    "Registered user can write and submit their blog for publishing on this site by filling out this form.",
+};
 
 export default function CreateBlogPageLayout({
   children,
