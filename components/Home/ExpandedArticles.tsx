9b7e257271a58067dd2e8d5a283aba7ac5efbd1e
OyedunOye <oyesinaoyedun@yahoo.com>
2025-05-06 21:51:34 +0200

    feat: Created a getAllAuthors service which retrieve all users with blogs published. They are displayed in the section listing authors on the blog's landing page.

--- components/Home/ExpandedArticles.tsx
+++ components/Home/ExpandedArticles.tsx
@@ -1,7 +1,14 @@
-import { authors, popularPostsList, tags, trendingTopics } from "@/constants";
+"use client";
+
+import {
+  authors,
+  categories,
+  popularPostsList,
+  trendingTopics,
+} from "@/constants";
 import { Button } from "../ui/button";
 import MaxWidth from "../common/MaxWidthWrapper";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import {
   Pagination,
   PaginationContent,
@@ -12,141 +19,118 @@ import {
   PaginationPrevious,
 } from "@/components/ui/pagination";
 import { ArticleCards } from "./ArticleCards";
+import { useGetAllAuthors } from "@/hooks/authors/useGetAllAuthors";
+import Loading from "../common/Loader";
+import AvatarRenderer from "../common/Avatar";
+import { useEffect, useState } from "react";
+
+interface Blogs {
+  // blogs: {
+  _id: string;
+  title: string;
+  blogContent: string;
+  articleImg: string;
+  category: string;
+  createdAt: string;
+  // };
+}
+
+interface Authors {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  authorImg: StaticImageData;
+  createdAt: string;
+  updatedAt: string;
+  blogs: Blogs[];
+}
 
 const ExpandedArticles = () => {
+  const { data, isLoading, isSuccess } = useGetAllAuthors();
+  const [allAuthors, setAllAuthors] = useState<Authors[]>([]);
+  const [topFiveAuthors, setTopFiveAuthors] = useState<Authors[]>([]);
+
+  useEffect(() => {
+    if (data) {
+      setAllAuthors(data.authors);
+      setTopFiveAuthors(data.authors.slice(0, 5));
+    }
+  }, [data]);
+
   return (
-    <section className="my-4">
-      <MaxWidth className="">
-        <h3 className="font-bold text-xl pb-3">🎉 Latest Articles</h3>
-        <div className="flex gap-4">
-          <div className="">
-            <ArticleCards />
-            <div className="flex justify-between my-3">
-              <Pagination className="justify-start mx-0 w-1/2">
-                <PaginationContent>
-                  <PaginationItem>
-                    <PaginationPrevious href="#" />
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationLink href="#">1</PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationEllipsis />
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationNext href="#" />
-                  </PaginationItem>
-                </PaginationContent>
-              </Pagination>
-
-              <Button variant="default">See more</Button>
-            </div>
-          </div>
-
-          <div className="flex flex-col w-1/2 gap-3">
+    <>
+      <section className="my-4">
+        <MaxWidth className="w-full">
+          <div className="flex flex-col  gap-3">
             <div className="flex flex-col border p-2 rounded-md bg-[#F3F4F6]">
-              <div className="flex justify-between">
-                <h4 className="font-bold text-md">🏷️ Discover more tags</h4>
-                <Button variant="ghost">View all</Button>
+              <div className="flex">
+                <h4 className="font-bold text-md">🏷️ Tags</h4>
               </div>
-              <div className="flex flex-wrap gap-2">
-                {tags.map((tag) => (
-                  <Button key={tag} variant="outline">
-                    {tag}
+              <div className="flex gap-4 my-4">
+                {categories.map(({ category }) => (
+                  <Button key={category} variant="outline">
+                    {category}
                   </Button>
                 ))}
               </div>
             </div>
 
-            {/* <div className="bg-[#F3F4F6] flex-col border p-2 rounded-md ">
-              <div className="flex justify-between">
-                <h4 className="font-bold text-md">✨ Trending topic</h4>
-                <Button variant="ghost">View all</Button>
-              </div>
-              <div className="flex flex-col gap-2">
-                {trendingTopics.map((topic) => (
-                  <div key={topic.category} className="my-2">
-                    <div className="flex gap-4 content-center">
-                      <Image
-                        src={topic.img}
-                        alt={topic.category}
-                        width={50}
-                        height={40}
-                      />
-                      <div className="flex flex-col gap-2 p-1">
-                        <h5 className="font-bold text-md content-center">
-                          {topic.category}
-                        </h5>
-                        <p className="text-sm text-gray-500 content-center">
-                          {topic.counter} Articles
-                        </p>
-                      </div>
+            {isSuccess ? (
+              <>
+                {data ? (
+                  <div className="bg-[#F3F4F6] flex-col border px-2 py-6 rounded-md">
+                    <div className="flex justify-between">
+                      <h4 className="font-bold text-md">✍️ Discover Authors</h4>
+                      <Button variant="ghost">View all</Button>
                     </div>
-                  </div>
-                ))}
-              </div>
-            </div> */}
-
-            <div className="bg-[#F3F4F6] flex-col border p-2 rounded-md">
-              <div className="flex justify-between">
-                <h4 className="font-bold text-md">✍️ Discover Authors</h4>
-                <Button variant="ghost">View all</Button>
-              </div>
-              <div className="flex flex-col gap-2">
-                {authors.map((author) => (
-                  <div key={author.name} className="my-2">
-                    <div className="flex gap-4 content-center">
-                      <Image
-                        src={author.img}
-                        alt={author.name}
-                        width={50}
-                        height={40}
-                      />
-                      <div className="flex flex-col gap-2">
-                        <h5 className="font-bold text-md content-center">
-                          {author.name}
-                        </h5>
-                        <p className="text-sm text-gray-500 content-center">
-                          {author.field}
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </div>
-            </div>
-
-            {/* <div className="bg-[#F3F4F6] flex-col border p-2 rounded-md">
-              <div className="flex justify-between">
-                <h4 className="font-bold text-md">🎯 Popular Posts</h4>
-                <Button variant="ghost">View all</Button>
-              </div>
-              <div className="flex flex-col gap-2 my-2">
-                {popularPostsList.map((post) => (
-                  <div key={post.topic} className="my-2">
-                    <div className="flex gap-4 content-center justify-between">
-                      <div className="flex flex-col gap-2">
-                        <div className="flex gap-1">
-                          <Image src={post.authImg} alt={post.topic} />
-                          <p className="text-gray-500 text-sm">{post.author}</p>
-                          <p className="text-gray-500 text-sm">{post.date}</p>
+                    <div className="flex flex-col gap-2">
+                      {topFiveAuthors.map((author: Authors) => (
+                        <div key={author.firstName} className="my-2">
+                          <div className="flex gap-4 content-center">
+                            {author.authorImg ? (
+                              <AvatarRenderer
+                                src={
+                                  "http://localhost:3001/" + author.authorImg
+                                }
+                                className="w-20 h-20"
+                              />
+                            ) : (
+                              <AvatarRenderer
+                                src={"http://localhost:3000/user-dummy.png"}
+                                className="h-20 w-20"
+                              />
+                            )}
+                            <div className="flex flex-col gap-2">
+                              <h5 className="font-bold text-md content-center capitalize">
+                                {author.firstName + " " + author.lastName}
+                              </h5>
+                              <p className="text-sm text-gray-500 content-center">
+                                {`Author of ${author.blogs[0].title}`}
+                              </p>
+                              <p className="text-sm text-gray-500 content-center">
+                                {/* {`${author.blogs.length} blogs`} */}
+                                {`${author.blogs.length} ${
+                                  author.blogs.length > 1 ? "blogs" : "blog"
+                                }`}
+                              </p>
+                            </div>
+                          </div>
                         </div>
-                        <h3 className="font-bold text-md content-center">
-                          {post.topic}
-                        </h3>
-                      </div>
-                      <div className="flex">
-                        <Image src={post.storyImg} alt={post.topic} />
-                      </div>
+                      ))}
                     </div>
                   </div>
-                ))}
-              </div>
-            </div> */}
+                ) : (
+                  <Loading message="No authors are available at the moment!" />
+                )}
+              </>
+            ) : (
+              <Loading message=" Loading author details" />
+            )}
           </div>
-        </div>
-      </MaxWidth>
-    </section>
+        </MaxWidth>
+      </section>
+    </>
   );
 };
 
